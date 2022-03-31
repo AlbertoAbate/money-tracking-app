8ff9474e4e52e2383f9a7d2cfeba72d1c23ba922
AlbertoAbate <AlbertoAbate@github.com>
2022-03-31 12:51:45 +0200

    Crate addOperation flow. Add saveWallet to store the wallet in the storage

--- src/js/models/Wallet.js
+++ src/js/models/Wallet.js
@@ -1,3 +1,7 @@
+var OpType = {
+    OUT: 0,
+    IN: 1
+}
 function getWallet() {
     var wallet = localStorage.getItem('wallet');
     if (!wallet) {
@@ -7,6 +11,9 @@ function getWallet() {
         }
     }
     return JSON.parse(wallet);
+}
+function saveWallet(wallet) {
+    localStorage.setItem('wallet', JSON.stringify(wallet));
 }
 
 function Wallet() {
@@ -18,8 +25,20 @@ function Wallet() {
         operations = wallet.operations;
     }
 
-    this.addOperation = function() {
-
+    this.addOperation = function(op) {
+        var operation = {
+            amount: op.amount,
+            description: op.description,
+            type: op.type,
+            date: new Date().getTime()
+        }
+        if(op.type === OpType.IN){
+           balance += operation.amount; 
+        }else if(op.type === OpType.OUT){
+            balance -= operation.amount; 
+        }
+        operations.push(operation);
+        saveWallet();
     }
     this.removeOperation = function() {
 
